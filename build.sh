baed85bc2daf39e91003b7a1e41ae9d549626c43
Ethan Buchman <ethan@coinculture.info>
2016-05-12 22:08:25 -0400

    make some figures tables. closes #26

--- build.sh
+++ build.sh
@@ -18,8 +18,8 @@ if [[  "$CHAPTER" == "" ]]; then
 	pdflatex main.tex
 else
 	pdflatex -jobname=chapBuild "\includeonly{$CHAPTER}\input{main.tex}"
-	bibtex chapBuild
-	pdflatex -jobname=chapBuild "\includeonly{$CHAPTER}\input{main.tex}"
-	pdflatex -jobname=chapBuild "\includeonly{$CHAPTER}\input{main.tex}"
+#	bibtex chapBuild
+#	pdflatex -jobname=chapBuild "\includeonly{$CHAPTER}\input{main.tex}"
+#	pdflatex -jobname=chapBuild "\includeonly{$CHAPTER}\input{main.tex}"
 fi
 
